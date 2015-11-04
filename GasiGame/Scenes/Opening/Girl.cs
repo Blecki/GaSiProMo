d7bb55f6d8851767a62c655a087ab0ebcc42e557
Blecki <jm@omnisu.com>
2015-11-04 08:32:43 -0500

    Switch from console to fake browser output.

--- GasiGame/Scenes/Opening/Girl.cs
+++ GasiGame/Scenes/Opening/Girl.cs
@@ -17,6 +17,8 @@ namespace Space.Scenes.Opening
 
             Long = "She is perhaps eight, with bangs and a smattering of freckles. She clutches the straps holding her to the flight couch tightly, in anticipation of the upcoming leap. She hasn't said a word the entire flight, that I've heard. Even now she has noticed my attention and is looking away.";
 
+            this.InitializeConversationTopics();
+
             this.PerformNoTopicsToDiscuss()
                 .Do((actor, girl) =>
                 {
